f6c36cf1488c7aa9ee816d1ffaddba91d0098eae
Jinpyo-laptop <neighbors28@naver.com>
2014-01-19 22:22:20 +0900

    Cleanup

--- Android.mk
+++ Android.mk
@@ -17,6 +17,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4
 
-LOCAL_PACKAGE_NAME := DokDoWallpapers
+LOCAL_PACKAGE_NAME := DokdoWallpapers
 
 include $(BUILD_PACKAGE)
